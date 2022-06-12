2fb6e6335f70e28556f57a8a7522093c1767ec05
Xavier DAVID <xavier.david@whitesmokegames.com>
2022-06-12 10:55:22 +0200

    commit to switch computer

--- Assets/Scripts/AbstractionScripts/Singleton.cs
+++ Assets/Scripts/AbstractionScripts/Singleton.cs
@@ -9,6 +9,7 @@ namespace Dyspra
     public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
     {
         private static T _instance;
+        [SerializeField] private bool shouldBeDestroyOnLoad = false; // If false, instance will be keep throughout all scenes
 
         protected virtual void Awake()
         {
@@ -18,25 +19,28 @@ namespace Dyspra
             }
             catch (Exception e)
             {
-                Debug.LogError("Error with singleton class. Exception " + e.Message);
+                Debug.LogError("Error with singleton class. Exception: " + e.Message);
             }
         }
 
         private void CheckInitInstance()
         {
-            if (FindObjectsOfType(typeof(T)).Length > 1)
+            if (FindObjectsOfType(typeof(T)).Length > 2)
                 throw new Exception("Something went really wrong " +
                                 " - there should never be more than 1 singleton!" +
                                 " Reopening the scene might fix it.");
             if (_instance == null)
             {
                 _instance = this as T;
-                DontDestroyOnLoad(this.gameObject);
+                if (shouldBeDestroyOnLoad == false)
+                    DontDestroyOnLoad(this.gameObject);
             }
             else
             {
-                Destroy(_instance);
+                Destroy(_instance.gameObject);
                 _instance = this as T;
+                Debug.Log("<color=yellow>Warning:</color> you've just " + 
+                    "instantiate a singleton twice. The first one has been destroyed");
             }
         }
     }
