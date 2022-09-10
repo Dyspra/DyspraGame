6bda15a68c810a1e22152d500fc10147457d8413
Baptiste ARRECOT <67862732+baptiste-arrecot@users.noreply.github.com>
2022-09-10 10:11:30 +0200

    [ADD] Adding the server that can send package
    TO DO:
    
    Fix the crash when too many packets are received

--- python_interface/dyspra_hand_tracking.py
+++ python_interface/dyspra_hand_tracking.py
@@ -9,6 +9,8 @@ from time import time
 def handtracking() -> None:
     videocap : encapsulated_cv2 = encapsulated_cv2(0)
     communicate : communication = communication(6542, "127.0.0.1")
+    date : float = 0
+
     if (videocap.isReady == False):
         return
     with mp_hands.Hands(
@@ -18,17 +20,25 @@ def handtracking() -> None:
       while videocap.videocap.isOpened():
         success, image = videocap.videocap.read()
         if not success:
-          print("Ignoring empty camera frame.")
+          # print("Ignoring empty camera frame.")
           continue
         image.flags.writeable = False
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         results = hands.process(image)
-        for idx, landmark in enumerate(results):
-              communicate.send_package(landmark.x, landmark.y, landmark.z, idx, time())
         if results.multi_hand_landmarks:
+          print(len(results.multi_hand_landmarks))
+          for hand in results.multi_hand_landmarks:
+              date = time()
+              for idx, landmark in enumerate(hand.landmark):
+                if idx <= 46:
+                  if (results.multi_handedness == 0):
+                    communicate.send_package(landmark.x, landmark.y, landmark.z, idx + 21, date)
+                  else:
+                    print(landmark)
+                    communicate.send_package(landmark.x, landmark.y, landmark.z, idx, date)
           print(results.multi_hand_landmarks)
-          image = draw_image(results, image)
-          videocap.display("Dyspra Test", image)
+          # image = draw_image(results, image)
+          # videocap.display("Dyspra Test", image)
         if cv2.waitKey(5) & 0xFF == 27:
           break
 if __name__ == '__main__':
--- python_interface/src/classes/communication.py
+++ python_interface/src/classes/communication.py
@@ -1,18 +1,36 @@
-from socket import socket, AF_INET, SOCK_DGRAM
+from socket import socket, AF_INET, SOCK_DGRAM, SOCK_STREAM
 from json import dumps
 from src.classes.interface import interface
+import sys
 
 class communication(interface):
     def __init__(self, port, hostname):
         self.socket = socket(AF_INET, SOCK_DGRAM)
         self.socket.bind((hostname, port))
+        client = self.socket.recvfrom(1024)
+        self.game = client[1]
+        print("Connection established with the client :", client[1])
     def __del__(self):
+        self.socket.sendto("Exit success", self.game)
         self.socket.close()
-    def send_package(self, x, y, z, landmark, image, date):
-        data = dumps({x: x, y: y, z: z, landmark: landmark, id: image, date: date})
+        print("Socket closed")
+    def send_package(self, x, y, z, landmark, date):
+        print(x, y, z, landmark)
+        data = dumps({x: x, y: y, z: z, landmark: landmark, date: date})
         try:
-            self.socket.sendall(bytes(data, encoding="utf-8"))
+            self.socket.sendto(bytes(data, encoding="utf-8"), self.game)
         except ValueError as e:
             print(e)
+            self.__del__()
+            sys.exit()
+    def send_ping(self):
+        data = self.recv_package(1024)
+        if data and data[0] and data[1] == self.game:
+            try:
+                self.socket.sendto(data[0], self.game)
+            except ValueError as e:
+                print(e)
+                self.__del__()
+                sys.exit()
     def recv_package(self, size : int):
         return self.socket.recvfrom(size)
