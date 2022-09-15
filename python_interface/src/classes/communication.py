1fa04c666ffa5562d2fefdb47ac1734e59906c97
Baptiste ARRECOT <67862732+baptiste-arrecot@users.noreply.github.com>
2022-09-15 15:29:57 +0200

    [FIX] Fixing the nb of indexes + code optimization on communication interface

--- python_interface/src/classes/communication.py
+++ python_interface/src/classes/communication.py
@@ -6,16 +6,16 @@ import sys
 class communication(interface):
     def __init__(self, port, hostname):
         self.socket = socket(AF_INET, SOCK_DGRAM)
-        self.socket.bind((hostname, port))
-        client = self.socket.recvfrom(1024)
-        self.game = client[1]
-        print("Connection established with the client :", client[1])
-    def __del__(self):
-        self.socket.sendto(bytes("Exit Success", encoding="utf-8"), self.game)
-        self.socket.close()
-        print("Socket closed")
+        # self.socket.bind((hostname, port))
+        # client = self.socket.recvfrom(1024)
+        self.game = (hostname, port)
+        print("Connection established with the client :", hostname, port, sep=" ")
+    # def __del__(self):
+    #     self.socket.sendto(bytes("Exit Success", encoding="utf-8"), self.game)
+    #     # self.socket.close()
+    #     print("Socket closed")
     def send_package(self, x, y, z, landmark, date):
-        print(x, y, z, landmark)
+        # print(x, y, z, landmark)
         data = dumps({"x": x, "y": y, "z": z, "landmark": landmark, "date": date})
         try:
             self.socket.sendto(bytes(data, encoding="utf-8"), self.game)
