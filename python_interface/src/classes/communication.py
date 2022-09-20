c7f80e67a4cc4f15729132f0a17eb2e032c62da8
Francis CLAIRICIA-ROSE-CLAIRE-JOSEPHINE <francis.clairicia-rose-claire-josephine@epitech.eu>
2022-09-21 00:41:00 +0200

    [ADD] Added a test for python interface

--- python_interface/src/classes/communication.py
+++ python_interface/src/classes/communication.py
@@ -1,26 +1,36 @@
-from socket import socket, AF_INET, SOCK_DGRAM, SOCK_STREAM
-from json import dumps
-from src.Classes.interface import interface
+from __future__ import annotations
+
 import sys
+from json import dumps
+from socket import AF_INET, SOCK_DGRAM, SOCK_STREAM, socket
+
+from src.classes.interface import interface
+
 
 class communication(interface):
-    def __init__(self, port, hostname):
+    def __init__(self, port, hostname, test_mode=False):
         self.socket = socket(AF_INET, SOCK_DGRAM)
-        self.socket.bind((hostname, port))
-        client = self.socket.recvfrom(1024)
-        # self.game = (hostname, port)
-        self.game = client[1]
+        if not test_mode:
+            self.socket.bind((hostname, port))
+            client = self.socket.recvfrom(1024)
+            # self.game = (hostname, port)
+            self.game = client[1]
+        else:
+            self.game = (hostname, port)
         print("Connection established with the client :", hostname, port, sep=" ")
+
     def __del__(self):
         self.socket.sendto(bytes("Exit Success", encoding="utf-8"), self.game)
         self.socket.close()
         print("Socket closed")
+
     def send_package(self, x, y, z, landmark, date):
         data = dumps({"x": x, "y": y, "z": z, "landmark": landmark, "date": date})
         try:
             self.socket.sendto(bytes(data, encoding="utf-8"), self.game)
         except (ValueError, OSError) as e:
             print(e)
+
     def send_ping(self):
         data = self.recv_package(1024)
         if data and data[0] and data[1] == self.game:
@@ -30,5 +40,6 @@ class communication(interface):
                 print(e)
                 self.__del__()
                 sys.exit()
-    def recv_package(self, size : int):
+
+    def recv_package(self, size: int):
         return self.socket.recvfrom(size)
--- python_interface/src/classes/interface.py
+++ python_interface/src/classes/interface.py
@@ -1,9 +1,15 @@
+from __future__ import annotations
+
+
 class interface:
     def __init__(self, port, hostname):
         pass
+
     def __del__(self):
         pass
+
     def send_package(self, x, y, z, landmark, image, date):
         pass
+
     def recv_package(self, size):
         pass
