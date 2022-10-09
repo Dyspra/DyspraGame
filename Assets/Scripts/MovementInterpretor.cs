3a003ada5062f9822c5e8f2f981d17f44bd62c20
Baptiste ARRECOT <67862732+baptiste-arrecot@users.noreply.github.com>
2022-10-09 12:42:06 +0200

    [MOD]Changing the way the data is sent to the server

--- Assets/Scripts/MovementInterpretor.cs
+++ Assets/Scripts/MovementInterpretor.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Net;
 using System.Net.Sockets;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Text;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class MovementInterpretor : MonoBehaviour
@@ -14,10 +16,19 @@ public class MovementInterpretor : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        tokenSource = new CancellationTokenSource();
-        var server = new UDPServer();
-        server.Initialize();
-        server.StartMessageLoop(tokenSource.Token);
+        // var files = Directory.EnumerateFiles("C:\\", "*dyspra_hand_tracking.py*", SearchOption.AllDirectories);
+        // string[] dirs = Directory.GetFiles(@"c:\", "*" + filetofind);
+        // tokenSource = new CancellationTokenSource();
+        // var server = new UDPServer();
+        // server.Initialize();
+        // server.StartMessageLoop(tokenSource.Token);
+        var path = Path.Combine(Application.persistentDataPath, "\\python_interface\\dyspra_hand_tracking.py");
+        UnityEngine.Debug.Log(Application.dataPath + "/python_interface/dyspra_hand_tracking.py");
+        if (File.Exists(Application.dataPath + "/python_interface/dyspra_hand_tracking.py")) {
+            Process.Start("python3.exe", "C:\\Users\\$2P2100-B0HQ5KT4OKR2\\Documents\\GitHub\\DyspraGame\\python_interface\\dyspra_hand_tracking.py 6542 127.0.0.1");
+        } else {
+            UnityEngine.Debug.Log("Python script does not exists in the current context");
+        }
     }
 
     // Update is called once per frame
@@ -28,6 +39,6 @@ public class MovementInterpretor : MonoBehaviour
 
     void OnDestroy()
     {
-        tokenSource.Cancel();
+        // tokenSource.Cancel();
     }
 }
