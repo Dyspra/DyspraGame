a44b6a248a4bb00572e515a11ea582cd0883715e
Baptiste ARRECOT <67862732+baptiste-arrecot@users.noreply.github.com>
2022-12-20 23:38:28 +0100

    [ADD] Readme + Arduino Bluetooth

--- Assets/Scripts/ArduinoController.cs
+++ Assets/Scripts/ArduinoController.cs
@@ -7,10 +7,11 @@ public class ArduinoController : MonoBehaviour
 {
     SerialPort arduino_port;
     public bool led_open = false;
+    public bool led_flash = false;
 
     void Start()
     {
-        arduino_port = new SerialPort("COM5", 9600);
+        arduino_port = new SerialPort("COM6", 9600);
         try {
             arduino_port.Open();
         } catch {
@@ -21,12 +22,29 @@ public class ArduinoController : MonoBehaviour
     {
         if (led_open) {
             open_led();
+        } else if (led_flash) {
+            flash_led();
+        } else {
+            close_led();
         }
     }
     void open_led()
     {
         if (arduino_port.IsOpen) {
             arduino_port.Write("1");
+        }
+    }
+    void close_led()
+    {
+        if (arduino_port.IsOpen) {
+            arduino_port.Write("2");
+        }
+    }
+    void flash_led()
+    {
+        if (arduino_port.IsOpen) {
+            arduino_port.Write("3");
+            arduino_port.Write("2");
         }
     }
 }
