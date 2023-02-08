a1c27f9f1ea9427c306519dc3c3033185c7d1474
Jimmy <jimmy.kohl@epitech.eu>
2023-02-08 13:56:58 +0100

    [MOD] Singleton Test to SingletonGameObject, move in Abstract Scripts folder
    [MOD] Clean Collision Detection scripts

--- Assets/Scripts/Haptics/CollisionDetection.cs
+++ Assets/Scripts/Haptics/CollisionDetection.cs
@@ -3,22 +3,13 @@ using System.Collections.Generic;
 
 public class CollisionDetection : MonoBehaviour
 {
-    public HapticDeviceManager hapticDeviceManager;
-    private string _collisionObjectName;
-    private Vector3 _collisionPoint;
-    private float _collisionIntensity;
-
     private void OnCollisionEnter(Collision collision)
     {
-
         foreach (ContactPoint contact in collision.contacts)
         {
             Debug.DrawRay(contact.point, contact.normal, Color.white, 10f);
         }
         Debug.Log("Collision detected with " + collision.gameObject.name + " with intensity " + collision.impulse.magnitude);
-        // _collisionObjectName = collision.gameObject.name;
-        // _collisionPoint = collision.contacts[0].point;
-        // _collisionIntensity = collision.impulse.magnitude;
 
         HapticDeviceManager.Instance.SendHapticData(collision.gameObject.name);
     }
@@ -32,11 +23,8 @@ public class CollisionDetection : MonoBehaviour
 
     private void OnCollisionStay(Collision collision)
     {
-        _collisionObjectName = collision.gameObject.name;
-        _collisionPoint = collision.contacts[0].point;
-        _collisionIntensity = collision.impulse.magnitude;
-        Debug.Log("Collision ongoing with " + _collisionObjectName + " at " + _collisionPoint + " with intensity " + _collisionIntensity);
+        Debug.Log("Collision with " + collision.gameObject.name + " is still happening");
 
-        HapticDeviceManager.Instance.SendHapticData(_collisionObjectName);
+        HapticDeviceManager.Instance.SendHapticData(collision.gameObject.name);
     }
 }
