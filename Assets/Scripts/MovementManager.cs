ad971a2eb73fed567b10e78bcd47cf26283ccae0
ThomasBillaud <thomasbillaud@epitech.eu>
2023-02-11 21:07:01 +0900

    [ADD] Wrist movement

--- Assets/Scripts/MovementManager.cs
+++ Assets/Scripts/MovementManager.cs
@@ -66,14 +66,34 @@ public class MovementManager : MonoBehaviour
             Vector3 newPos = new Vector3(hp.packages[i + 21].position.x * -5, hp.packages[i + 21].position.y* 5, hp.packages[i + 21].position.z * 5);
             LeftHandPoints[i].transform.localPosition = Vector3.Lerp(LeftHandPoints[i].transform.localPosition, newPos, Time.deltaTime * speed);
         }
-        RotateFingers();
+        RotateFingers(hp);
     }
 
-    public void RotateFingers()
-    {
-        Vector3 dir1 = RightHandPoints[0].transform.position - RightHandPoints[5].transform.position;
-        Vector3 dir2 = RightHandPoints[5].transform.position - RightHandPoints[6].transform.position;
+    public void RotateFingers(HandPosition hp)
+    {   
+        Vector3 newPos = new Vector3(hp.packages[0].position.x * -1, hp.packages[0].position.y, hp.packages[0].position.z);
+        R_0.transform.position = Vector3.Lerp(R_0.transform.position, newPos, Time.deltaTime * speed);
+        RotateFinger(R_2,  hp.packages[0].position - hp.packages[2].position,  hp.packages[2].position -  hp.packages[3].position);
+        //RotateFinger(R_3,  hp.packages[2].position - hp.packages[3].position,  hp.packages[3].position -  hp.packages[4].position);
+        RotateFinger(R_5,  hp.packages[0].position - hp.packages[5].position,  hp.packages[5].position -  hp.packages[6].position);        
+        //RotateFinger(R_6,  hp.packages[5].position - hp.packages[6].position,  hp.packages[6].position -  hp.packages[7].position);
+        //RotateFinger(R_7,  hp.packages[6].position - hp.packages[7].position,  hp.packages[7].position -  hp.packages[8].position);
+        RotateFinger(R_9,  hp.packages[0].position - hp.packages[9].position,  hp.packages[9].position -  hp.packages[10].position);
+        //RotateFinger(R_10, hp.packages[9].position - hp.packages[10].position, hp.packages[10].position - hp.packages[11].position);
+        //RotateFinger(R_11, hp.packages[10].position -hp.packages[11].position, hp.packages[11].position - hp.packages[12].position);
+        RotateFinger(R_13, hp.packages[0].position - hp.packages[13].position, hp.packages[13].position - hp.packages[14].position);
+        //RotateFinger(R_14, hp.packages[13].position -hp.packages[14].position, hp.packages[14].position - hp.packages[15].position);
+        //RotateFinger(R_15, hp.packages[14].position -hp.packages[15].position, hp.packages[15].position - hp.packages[16].position);
+        RotateFinger(R_17, hp.packages[0].position - hp.packages[17].position, hp.packages[17].position - hp.packages[18].position);
+        //RotateFinger(R_18, hp.packages[17].position -hp.packages[18].position, hp.packages[18].position - hp.packages[19].position);
+        //RotateFinger(R_19, hp.packages[18].position -hp.packages[19].position, hp.packages[19].position - hp.packages[20].position);
+    }
 
-        R_5.transform.localRotation = Quaternion.Inverse(Quaternion.FromToRotation(dir1, dir2));
+    public void RotateFinger(GameObject joint, Vector3 dir1, Vector3 dir2)
+    {
+        Vector3 newDir1 = new Vector3(dir1.x * -5, dir1.y * 5, dir1.z * 5);
+        Vector3 newDir2 = new Vector3(dir1.x * -5, dir1.y * 5, dir1.z * 5);
+
+        joint.transform.localRotation = Quaternion.Inverse(Quaternion.FromToRotation(newDir1, newDir2));
     }
 }
