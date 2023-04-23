62b71fd4cd896e2e31aec852289b311d5e7acfca
titouandelpech <63298260+titouandelpech@users.noreply.github.com>
2023-04-23 21:40:40 +0900

    [ADD/FIX] attraction cone working and attracting animation fixed

---
+++ Assets/Scripts/HandModel/HandsAttraction/HandleAttractionCone.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleAttractionCone : MonoBehaviour
+{
+    MeshCollider coneCollider;
+    [SerializeField] AttractToTarget AttractToTarget;
+    private void Awake()
+    {
+        coneCollider = GetComponent<MeshCollider>();
+        AttractToTarget = FindObjectOfType<AttractToTarget>();
+    }
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Enter trigger " + other.name);
+        if (other.gameObject.name != "hand_2" && other.gameObject.TryGetComponent<Rigidbody>(out var rB))
+            AttractToTarget.SetRigidbodyAttracted(rB);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Exit trigger " + other.name);
+        if (other.gameObject.name != "hand_2" && other.gameObject.TryGetComponent<Rigidbody>(out var rB))
+            AttractToTarget.RemoveRigidbodyAttracted(rB);
+    }
+}
