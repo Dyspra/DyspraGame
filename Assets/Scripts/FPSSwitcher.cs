b1ab6eb0f494ced3ef7f56d5bdb5021e4486a733
Aurélien LIN <rilong.lin@epitech.eu>
2023-05-24 19:45:33 +0900

    [CHANGE] Settings UI

---
+++ Assets/Scripts/FPSSwitcher.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Collections.Generic;
+
+public class FPSSwitcher : MonoBehaviour
+{
+    public TMP_Dropdown fpsDropdown;
+
+    private Dictionary<string, int> fpsOptions = new Dictionary<string, int>
+    {
+        { "60 FPS", 60 },
+        { "120 FPS", 120 },
+        { "180 FPS", 180 },
+        { "320 FPS", 320 }
+    };
+
+    private void Start()
+    {
+        fpsDropdown.AddOptions(new List<string>(fpsOptions.Keys));
+        fpsDropdown.SetValueWithoutNotify(GetFPSIndex(Application.targetFrameRate));
+        fpsDropdown.onValueChanged.AddListener(OnDropdownValueChanged);
+    }
+
+    private void OnDropdownValueChanged(int index)
+    {
+        string selectedOption = fpsDropdown.options[index].text;
+        int selectedFPS = fpsOptions[selectedOption];
+
+        Application.targetFrameRate = selectedFPS;
+    }
+
+    private int GetFPSIndex(int fps)
+    {
+        foreach (var kvp in fpsOptions)
+        {
+            if (kvp.Value == fps)
+                return fpsDropdown.options.FindIndex(option => option.text == kvp.Key);
+        }
+
+        return 0;
+    }
+}
