bfc866242eab447f253757f6803ac890a0fc8323
ThomasBillaud <thomasbillaud@epitech.eu>
2023-05-28 22:23:36 +0900

    [ADD] Result menu + pause menu + baked light in first exercise

---
+++ Assets/Scripts/UI/PauseButton.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseButton : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseMenu.SetActive(true);
+        }
+    }
+}
