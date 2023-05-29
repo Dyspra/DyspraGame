78734123c05621a71cda4773f839eee3c2ae3044
ThomasBillaud <thomasbillaud@epitech.eu>
2023-05-29 18:02:58 +0900

    [FIX] Pause menu was stopping the whole game when launching

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-    void OnEnable()
-    {
-        Time.timeScale = 0f;
-    }
-
     void OnDisable()
     {
         Time.timeScale = 1f;
