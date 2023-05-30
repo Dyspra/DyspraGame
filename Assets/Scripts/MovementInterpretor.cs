f90fd6e3830c532b563b26636c42697a7eb3d0eb
Jimmy <jimmy.kohl@epitech.eu>
2023-05-31 01:28:08 +0200

    🚀 feat(MovementInterpretor.cs): add support for building Mediapipe Python Interface during Unity build process
    🐛 fix(MovementInterpretor.cs): fix typo in ConvertToPlatformPath method parameter name
    ✨ feat(MovementInterpretor.cs): make ConvertToPlatformPath method static to avoid unnecessary object creation

--- Assets/Scripts/MovementInterpretor.cs
+++ Assets/Scripts/MovementInterpretor.cs
@@ -10,21 +10,28 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
-using UnityEditor;
-
+#if UNITY_EDITOR
+   using UnityEditor;
+   using UnityEditor.Build;
+   using UnityEditor.Build.Reporting;
+#endif
 
 public class MovementInterpretor : MonoBehaviour
+#if UNITY_EDITOR
+   , IPreprocessBuildWithReport
+#endif
 {
-   private bool isWindows = System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows);
+   private static bool isWindows = System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows);
 
    #if UNITY_EDITOR
       // private string specPath = Path.Combine(Application.dataPath, "Plugins/MediapipePythonInterface/dyspra_hand_tracking.spec");
       // private string tempPath = Path.Combine(Application.persistentDataPath, "MediapipePythonInterface/build");
       // private string distPath = Path.Combine(Application.persistentDataPath, "MediapipePythonInterface/dist");
-      private string _binaryPath = this.ConvertToPlatformPath(Path.Combine(Application.persistentDataPath, "MediapipePythonInterface/dist/dyspra_hand_tracking/dyspra_hand_tracking"));
+      private string _binaryPath;
 
       void Awake()
       {
+         _binaryPath = ConvertToPlatformPath(Path.Combine(Application.persistentDataPath, "MediapipePythonInterface/dist/dyspra_hand_tracking/dyspra_hand_tracking"));
          StartUDPServer();
          UnityEngine.Debug.Log("isWindows: " + isWindows);
          UnityEngine.Debug.Log("_executablePath: " + _executablePath);
@@ -52,15 +59,82 @@ public class MovementInterpretor : MonoBehaviour
          }
       }
 
-      void OnPostprocessBuild(BuildReport report)
-      {
-         Debug.Log("OnPostprocessBuild for target " + report.summary.platform + " at path " + report.summary.outputPath);
+      public int callbackOrder { get { return 0; } }
 
-         // Build the python script
-         bool success = this.BuildPythonScript();
+      // public void OnPreprocessBuild(BuildReport report)
+      // {
+         
+
+      //    UnityEngine.Debug.Log("OnPostprocessBuild for target " + report.summary.platform + " at path " + report.summary.outputPath);
+      //    UnityEngine.Debug.Log("Data folder of output path: " + Path.Combine(Path.GetDirectoryName(report.summary.outputPath), Path.GetFileNameWithoutExtension(report.summary.outputPath) + "_Data"));
+         
+      //    this.BuildPythonScript(
+      //          ConvertToPlatformPath(Path.Combine(Application.dataPath, "Plugins/MediapipePythonInterface/dyspra_hand_tracking.spec")),
+      //          ConvertToPlatformPath(Path.Combine(Application.persistentDataPath, "MediapipePythonInterface/build")),
+      //          ConvertToPlatformPath(Path.Combine(Path.GetDirectoryName(report.summary.outputPath), Path.GetFileNameWithoutExtension(report.summary.outputPath) + "_Data/Plugins/MediapipePythonInterface"))
+      //    ).ContinueWith((task) => {
+      //       UnityEngine.Debug.Log("OnPostprocessBuild: BuildPythonScript continuewith task.Result: " + task.Result);
+      //    });
+      // }
+public void OnPreprocessBuild(BuildReport report)
+{
+      UnityEngine.Debug.Log("OnPreprocessBuild for target " + report.summary.platform + " at path " + report.summary.outputPath);
+      UnityEngine.Debug.Log("Data folder of output path: " + Path.Combine(Path.GetDirectoryName(report.summary.outputPath), Path.GetFileNameWithoutExtension(report.summary.outputPath) + "_Data"));
+
+      // Display the dialog and get the result
+      bool result = EditorUtility.DisplayDialog("Build Mediapipe python interface", "Do you want to build the Mediapipe Python Interface?", "Yes", "No");
+
+      // Check if the result is "No"
+      if (!result)
+      {
+         // Cancel the BuildPythonScript
+         return;
+      }
+      // Display the progress bar
+      bool cancelled = false;
+      EditorUtility.DisplayCancelableProgressBar("Build Mediapipe python interface", "Building Mediapipe Python Interface...", 0.0f);
+
+      var ts = new CancellationTokenSource();
+      CancellationToken ct = ts.Token;
+      Task<bool> buildPythonScriptTask = this.BuildPythonScript(
+         ConvertToPlatformPath(Path.Combine(Application.dataPath, "Plugins/MediapipePythonInterface/dyspra_hand_tracking.spec")),
+         ConvertToPlatformPath(Path.Combine(Application.persistentDataPath, "MediapipePythonInterface/build")),
+         ConvertToPlatformPath(Path.Combine(Path.GetDirectoryName(report.summary.outputPath), Path.GetFileNameWithoutExtension(report.summary.outputPath) + "_Data/Plugins/MediapipePythonInterface")),
+         ct
+      );
+
+      // Update the progress bar while the build is running
+      while (!buildPythonScriptTask.IsCompleted && !cancelled)
+      {
+         cancelled = EditorUtility.DisplayCancelableProgressBar("Build Mediapipe python interface", "Building Mediapipe Python Interface...", 0.5f);
       }
 
-      Task<bool> BuildPythonScript(string specPath, string tempPath, string distPath)
+      // Hide the progress bar
+      EditorUtility.ClearProgressBar();
+
+      if (cancelled)
+      {
+         // Cancel the BuildPythonScript
+         buildPythonScriptTask.ContinueWith((task) => {
+            UnityEngine.Debug.Log("OnPreprocessBuild: BuildPythonScript cancelled");
+         });
+         return;
+      }
+
+      // Check if the build was successful
+      if (buildPythonScriptTask.Result)
+      {
+         UnityEngine.Debug.Log("OnPreprocessBuild: BuildPythonScript finished successfully");
+         EditorUtility.DisplayDialog("Build Python Script", "BuildPythonScript finished successfully", "OK");
+      }
+      else
+      {
+         UnityEngine.Debug.LogError("OnPreprocessBuild: BuildPythonScript failed");
+         EditorUtility.DisplayDialog("Build Python Script", "BuildPythonScript failed", "OK");
+      }
+}
+
+      Task<bool> BuildPythonScript(string specPath, string tempPath, string distPath, CancellationToken ct = null)
       { 
          var tcs = new TaskCompletionSource<bool>();
          try {
@@ -70,13 +144,17 @@ public class MovementInterpretor : MonoBehaviour
                UnityEngine.Debug.Log("Python script does not exists in the current context");
                return Task.FromResult(false);
             }
-         UnityEngine.Debug.Log("Building Python script in " + distPath);
+            UnityEngine.Debug.Log("Building Python script in " + distPath);
+            if (!Directory.Exists(distPath))
+            {
+               Directory.CreateDirectory(distPath);
+            }
 
             var buildProcess = new Process
             {
                StartInfo = {
                   FileName = "pyinstaller",
-                  Arguments = "--workpath " + tempPath + "--distpath " + distPath + " --clean --noconfirm " + specPath,
+                  Arguments = "--workpath " + tempPath + " --distpath " + distPath + " --clean --noconfirm " + specPath,
                   UseShellExecute = false,
                   RedirectStandardOutput = true,
                   RedirectStandardError = true,
@@ -90,7 +168,7 @@ public class MovementInterpretor : MonoBehaviour
             };
             buildProcess.OutputDataReceived += (sender, args) => UnityEngine.Debug.Log("[MediapipePythonInterface installer] " + args.Data);
             // todo: normal log goes to stderr instead of stdout
-            buildProcess.ErrorDataReceived += (sender, args) => UnityEngine.Debug.LogError("[MediapipePythonInterface installer] " + args.Data);
+            buildProcess.ErrorDataReceived += (sender, args) => UnityEngine.Debug.Log("[MediapipePythonInterface installer stderr] " + args.Data);
             UnityEngine.Debug.Log("buildProcess start");
             bool started = buildProcess.Start();
             UnityEngine.Debug.Log("buildProcess.Started: " + started);
@@ -101,6 +179,14 @@ public class MovementInterpretor : MonoBehaviour
             }
             buildProcess.BeginOutputReadLine();
             buildProcess.BeginErrorReadLine();
+            if (ct != null)
+            {
+               ct.Register(() => {
+                  UnityEngine.Debug.Log("buildProcess ct.Register");
+                  buildProcess.Kill();
+                  tcs.SetResult(false);
+               });
+            }
          } catch (Exception e) {
             UnityEngine.Debug.LogError("MediapipePythonInterface install Exception: " + e);
             tcs.SetResult(false);
@@ -109,10 +195,11 @@ public class MovementInterpretor : MonoBehaviour
             return tcs.Task;
       }
    #elif UNITY_STANDALONE
-      private string _binaryPath = this.ConvertToPlatformPath(Path.Combine(Application.dataPath, "Plugins/MediapipePythonInterface/dyspra_hand_tracking"));
+      private string _binaryPath;
 
       void Awake()
       {
+         _binaryPath = ConvertToPlatformPath(Path.Combine(Application.dataPath, "Plugins/MediapipePythonInterface/dyspra_hand_tracking"));
          this.StartUDPServer();
          this.LaunchPythonScript();
       }
@@ -153,7 +240,7 @@ public class MovementInterpretor : MonoBehaviour
          var process = new Process
          {
             StartInfo = {
-               WorkingDirectory = Path.Combine(Path.GetFullPath(Path.Combine(Application.persistentDataPath, "MediapipePythonInterface/dist/dyspra_hand_tracking")).TrimEnd(Path.DirectorySeparatorChar)),
+               WorkingDirectory = Path.Combine(Path.GetFullPath(Path.Combine(Application.persistentDataPath, "MediapipePythonInterface/dist/dyspra_hand_tracking")).TrimEnd(Path.DirectorySeparatorChar)), // todo: make it platform agnostic
                FileName = _executablePath,
                Arguments = "5000 127.0.0.1", // todo: when 5000 is not available, change it to another port
                UseShellExecute = false,
@@ -190,13 +277,13 @@ public class MovementInterpretor : MonoBehaviour
    {
       tokenSource.Cancel();
    }
-   private string ConvertToPlatformPath(string path, bool isExecutablePath = false)
+   private static string ConvertToPlatformPath(string path, bool isExecutablePath = false)
    {
       if (isWindows == true)
       {
          path = path.Replace("/", "\\");
       }
-      if (isExecutable == true)
+      if (isExecutablePath == true)
       {
          path += ".exe";
       }
