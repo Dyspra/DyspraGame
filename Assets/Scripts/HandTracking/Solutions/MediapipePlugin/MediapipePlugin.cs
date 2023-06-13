c102ae452974787a08332e69bf835589b6bbdd10
Jimmy <jimmy.kohl@epitech.eu>
2023-06-13 13:08:24 +0200

    ✨ feat(hand-tracking): add two new solutions for hand tracking

---
+++ Assets/Scripts/HandTracking/Solutions/MediapipePlugin/MediapipePlugin.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class MediaPipePlugin : MonoBehaviour, IHandTrackingSolution
+{
+    public string id => "mediapipe-plugin";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
