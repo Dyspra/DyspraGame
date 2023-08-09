fb7bb56010af9c38721f41f9c0266f37062899e8
Jimmy <jimmy.kohl@epitech.eu>
2023-08-09 13:52:01 +0200

    🔧 fix(AnalyticsManager.cs): change AskForConsent method visibility to private for encapsulation and improved code organization ✨ feat(AnalyticsManager.cs): add LogEvent method to log custom events with a name and a dictionary of parameters ✨ feat(AnalyticsManager.cs): add LogExerciseStarted method to log when the player starts an exercise with levelId and date parameters ✨ feat(AnalyticsManager.cs): add LogExerciseStop method to log when the player stops an exercise with levelId, score, reason, and date parameters

--- Assets/Scripts/Singletons/AnalyticsManager.cs
+++ Assets/Scripts/Singletons/AnalyticsManager.cs
@@ -19,7 +19,7 @@ public class AnalyticsManager : SingletonGameObject<AnalyticsManager>
         AskForConsent();
     }
 
-    void AskForConsent()
+    private void AskForConsent()
     {
         // yes yes very consent
         ConsentGivent();
@@ -34,5 +34,46 @@ public class AnalyticsManager : SingletonGameObject<AnalyticsManager>
     {
         UnityEngine.Debug.Log("SetUserId: " + userId);
         UnityServices.ExternalUserId = userId;
+    }
+
+    /// <summary>
+    ///  Log an event with a name and a dictionary of parameters.
+    /// </summary>
+    /// <param name="eventName"></param>
+    /// <param name="eventData"></param>
+    public void LogEvent(string eventName, Dictionary<string, object> eventData)
+    {
+        UnityEngine.Debug.Log("LogEvent: " + eventName);
+        AnalyticsService.Instance.SendCustomEvent(eventName, eventData);
+    }
+
+    // Built-in custom events:
+
+    /// <summary>
+    /// Log when the player starts an exercise.
+    /// </summary>
+    /// <param name="levelId"></param>
+    public void LogExerciseStarted(string levelId)
+    {
+        LogEvent("exerciseStarted", new Dictionary<string, object> {
+            { "levelId", levelId },
+            { "date", System.DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ssZ") }
+        });
+    }
+
+    /// <summary>
+    /// Log when the player stops an exercise.
+    /// </summary>
+    /// <param name="levelId">The ID of the exercise level.</param>
+    /// <param name="score">The score achieved by the player.</param>
+    /// <param name="reason">The reason for stopping the exercise. Can be "quit", "complete", or "failed".</param>
+    public void LogExerciseStop(string levelId, int score, string reason)
+    {
+        LogEvent("exerciseStop", new Dictionary<string, object> {
+            { "levelId", levelId },
+            { "score", score },
+            { "reason", reason },
+            { "date", System.DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ssZ") }
+        });
     }
 }
