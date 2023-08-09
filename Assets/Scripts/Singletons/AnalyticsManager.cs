e277ef94c4592b3946ee11bf17625425c63ee514
Jimmy <jimmy.kohl@epitech.eu>
2023-08-09 16:29:03 +0200

    🐛 fix(BDD UI Scene.unity): update m_IndirectSpecularColor values to improve accuracy ✨ feat(BDD UI Scene.unity): add new GameObject LoadManager to the scene
    🐛 fix(AnalyticsManager.cs): change LogExerciseStarted parameter name from levelId to exerciseId for consistency
    🐛 fix(AnalyticsManager.cs): change LogExerciseStop parameter type from string to ExerciseConstants.E_QuitReason enum for type safety
    ✨ feat(AnalyticsManager.cs): add new methods to log events for opening settings, changing screen mode, changing screen resolution, changing volume, changing quality, changing FPS limit, changing hand tracking solution, opening exercise list, opening exercise history, starting calibration, finishing calibration, starting tutorial, finishing tutorial, finishing exercise step, replaying exercise, and quitting exercise after finishing

--- Assets/Scripts/Singletons/AnalyticsManager.cs
+++ Assets/Scripts/Singletons/AnalyticsManager.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Unity.Services.Core;
 using Unity.Services.Analytics;
 using System.Collections.Generic;
 using Unity.Services.Core.Environments;	
+using Constants;
 
 public class AnalyticsManager : SingletonGameObject<AnalyticsManager>
 {
@@ -36,28 +38,43 @@ public class AnalyticsManager : SingletonGameObject<AnalyticsManager>
         UnityServices.ExternalUserId = userId;
     }
 
+    // detect when a scene is loaded
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        UnityEngine.Debug.Log("OnSceneLoaded: " + scene.name);
+        UnityEngine.Debug.Log(mode);
+        LogEvent("sceneLoaded", new Dictionary<string, object> {
+            { "sceneName", scene.name },
+            { "sceneMode", mode.ToString() }
+        });
+    }
+
     /// <summary>
     ///  Log an event with a name and a dictionary of parameters.
     /// </summary>
     /// <param name="eventName"></param>
     /// <param name="eventData"></param>
-    public void LogEvent(string eventName, Dictionary<string, object> eventData)
+    private void LogEvent(string eventName, Dictionary<string, object> eventData)
     {
         UnityEngine.Debug.Log("LogEvent: " + eventName);
-        AnalyticsService.Instance.SendCustomEvent(eventName, eventData);
+        AnalyticsService.Instance.CustomData(eventName, eventData);
     }
 
-    // Built-in custom events:
+    // ============= Built-in custom events:
 
     /// <summary>
     /// Log when the player starts an exercise.
     /// </summary>
     /// <param name="levelId"></param>
-    public void LogExerciseStarted(string levelId)
+    public void LogExerciseStarted(string exerciseId)
     {
         LogEvent("exerciseStarted", new Dictionary<string, object> {
-            { "levelId", levelId },
-            { "date", System.DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ssZ") }
+            { "exerciseId", exerciseId }
         });
     }
 
@@ -67,13 +84,124 @@ public class AnalyticsManager : SingletonGameObject<AnalyticsManager>
     /// <param name="levelId">The ID of the exercise level.</param>
     /// <param name="score">The score achieved by the player.</param>
     /// <param name="reason">The reason for stopping the exercise. Can be "quit", "complete", or "failed".</param>
-    public void LogExerciseStop(string levelId, int score, string reason)
+    public void LogExerciseStop(string levelId, int score, ExerciseConstants.E_QuitReason reason)
     {
         LogEvent("exerciseStop", new Dictionary<string, object> {
-            { "levelId", levelId },
-            { "score", score },
-            { "reason", reason },
-            { "date", System.DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ssZ") }
+            { "exerciseId", levelId },
+            { "userScore", score },
+            { "stopReason", reason.ToString() }
+        });
+    }
+
+    public void LogOpenSettings(string from)
+    {
+        LogEvent("openSettings", new Dictionary<string, object> {
+            { "from", from }
+        });
+    }
+
+    /// <summary>
+    /// Logs when the player changes the screen mode.
+    /// </summary>
+    /// <param name="screenMode">The screen mode. Can be "FULLSCREEN" or "WINDOWED".</param>
+    public void LogS_ChangeScreenMode(string screenMode)
+    {
+        LogEvent("S_changeScreenMode", new Dictionary<string, object> {
+            { "screenMode", screenMode },
+        });
+    }
+
+    public void LogS_ChangeScreenResolution(int screenWidth, int screenHeight)
+    {
+        LogEvent("S_changeScreenResolution", new Dictionary<string, object> {
+            { "screenWidth", screenWidth },
+            { "screenHeight", screenHeight }
+        });
+    }
+
+    public void LogS_ChangeVolume(string volumeType, float volume)
+    {
+        LogEvent("S_changeVolume", new Dictionary<string, object> {
+            { "volumeType", volumeType },
+            { "volume", volume }
+        });
+    }
+    public void LogS_ChangeQuality(string quality)
+    {
+        LogEvent("S_changeQuality", new Dictionary<string, object> {
+            { "graphicQuality", quality }
+        });
+    }
+
+    public void LogS_ChangeFPSLimit(int fpsLimit)
+    {
+        LogEvent("S_changeFPSLimit", new Dictionary<string, object> {
+            { "fpsLimit", fpsLimit }
+        });
+    }
+
+    public void LogS_ChangeHandTrackingSolution(string handTrackingSolution)
+    {
+        LogEvent("S_changeHandTrackingSolution", new Dictionary<string, object> {
+            { "handTrackingSolution", handTrackingSolution }
+        });
+    }
+
+    public void LogOpenExerciseList()
+    {
+        LogEvent("openExerciseList", new Dictionary<string, object> {
+        });
+    }
+
+    public void LogOpenExerciseHistory()
+    {
+        LogEvent("openExerciseHistory", new Dictionary<string, object> {
+        });
+    }
+
+    public void LogEx1_CalibrationStarted()
+    {
+        LogEvent("ex1_calibrationStart", new Dictionary<string, object> {
+        });
+    }
+
+    public void LogEx1_CalibrationFinished()
+    {
+        LogEvent("ex1_calibrationFinish", new Dictionary<string, object> {
+        });
+    }
+
+    public void LogEx1_TutorialStarted()
+    {
+        LogEvent("ex1_tutorialStart", new Dictionary<string, object> {
+        });
+    }
+
+    public void LogEx1_TutorialFinished()
+    {
+        LogEvent("ex1_tutorialFinish", new Dictionary<string, object> {
+        });
+    }
+
+    public void LogEx1_StepFinished(int step, int score)
+    {
+        LogEvent("ex1_stepFinish", new Dictionary<string, object> {
+            { "exerciseStep", step },
+            { "userScore", score }
+        });
+    }
+
+    public void LogExerciseReplay(string exerciseId)
+    {
+        LogEvent("exerciseReplay", new Dictionary<string, object> {
+            { "exerciseId", exerciseId }
+        });
+    }
+
+    public void LogExerciseQuitAfterFinished(string exerciseId)
+    {
+        LogEvent("exerciseQuitAfterFinished", new Dictionary<string, object> {
+            { "exerciseId", exerciseId }
         });
     }
 }
