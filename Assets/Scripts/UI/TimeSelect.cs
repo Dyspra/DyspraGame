e29a68bb1771dc8f9b3febbcbd1ba6bfd7e8c3a5
Thomas Billaud <thomas.billaud@epitech.eu>
2023-09-08 10:47:44 +0200

    [MOD] Timer in exercise 2 is now going from 5min to 5min instead of 1s to 1s

--- Assets/Scripts/UI/TimeSelect.cs
+++ Assets/Scripts/UI/TimeSelect.cs
@@ -5,12 +5,15 @@ public class TimeSelect : MonoBehaviour
 {
     public Slider timeSlider;
     public TMP_Text selectedTimeText;
-    public int selectedTime = 60;
+    public int selectedTime = 5;
+    public int stepAmount = 5;
+    private int numberOfSteps = 0;
 
     private void Start()
     {
         // Set initial value and update text
         timeSlider.value = selectedTime;
+        numberOfSteps = (int)timeSlider.maxValue / stepAmount;
         UpdateSelectedTime();
     }
 
@@ -21,7 +24,10 @@ public class TimeSelect : MonoBehaviour
 
     public void UpdateSelectedTime()
     {
-        selectedTime = Mathf.RoundToInt(timeSlider.value);
-        selectedTimeText.text = "Temps choisi: " + selectedTime + " secondes";
+        float range = (timeSlider.value / timeSlider.maxValue) * numberOfSteps;
+        int ceil = Mathf.CeilToInt(range);
+        timeSlider.value = ceil * stepAmount;
+        selectedTime = ceil * stepAmount * 60;
+        selectedTimeText.text = "Temps choisi: " + ceil * stepAmount + " minutes";
     }
 }
