22991fd62116fa7069cba98d0e4c39bbc5820a31
Thomas Billaud <thomas.billaud@epitech.eu>
2023-09-10 15:55:20 +0200

    [ADD] Sounds to jellyfishes + new light around the jellyfish so it looks great around every angle

--- Assets/Scripts/Jellyfish/JellyfishBehaviour.cs
+++ Assets/Scripts/Jellyfish/JellyfishBehaviour.cs
@@ -6,6 +6,7 @@ public class JellyfishBehaviour : AJellyfishBehaviour
 {
     public Material lighted_mat;
     public Material immun_mat;
+    public MultipleAudioSource audioSource;
 
     private void OnCollisionEnter(Collision other) {
         if (other.gameObject.tag == "Yellow") {
@@ -13,23 +14,27 @@ public class JellyfishBehaviour : AJellyfishBehaviour
             score.UpdateScore(ToAdd);
             ChangeColor(lighted_mat);
             isLightUp = true;
+            audioSource.sound1.Play();
         }
         if (other.gameObject.tag == "Red" && isInvincible == false && isLightUp == true) {
             ToAdd = -1;
             score.UpdateScore(ToAdd);
             ChangeColor(base_mat);
             isLightUp = false;
+            audioSource.sound2.Play();
         }
         if (other.gameObject.tag == "Blue" && isLightUp == true) {
             isInvincible = true;
             ChangeColor(immun_mat);
             StartCoroutine(Timer());
+            audioSource.sound3.Play();
         }
         if (other.gameObject.tag == "Green" && isLightUp == false && other.gameObject.GetComponent<JellyfishBehaviour>().isLightUp == true) {
             ToAdd = 1;
             score.UpdateScore(ToAdd);
             isLightUp = true;
-        }
+			audioSource.sound1.Play();
+		}
     }
     protected void ChangeColor(Material mat)
     {
