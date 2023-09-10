319100f1be1e0982a4a5c513e4c3fcc52b54c933
Thomas Billaud <thomas.billaud@epitech.eu>
2023-09-10 17:22:53 +0200

    [MOD] Jellyfish light turns on and off when touching another jellyfish

--- Assets/Scripts/Jellyfish/JellyfishBehaviour.cs
+++ Assets/Scripts/Jellyfish/JellyfishBehaviour.cs
@@ -7,20 +7,21 @@ public class JellyfishBehaviour : AJellyfishBehaviour
     public Material lighted_mat;
     public Material immun_mat;
     public MultipleAudioSource audioSource;
+    public List<GameObject> lights;
 
     private void OnCollisionEnter(Collision other) {
         if (other.gameObject.tag == "Yellow") {
             ToAdd = 1;
             score.UpdateScore(ToAdd);
-            ChangeColor(lighted_mat);
-            isLightUp = true;
+            //ChangeColor(lighted_mat);
+            ChangeLight();
             audioSource.sound1.Play();
         }
         if (other.gameObject.tag == "Red" && isInvincible == false && isLightUp == true) {
             ToAdd = -1;
             score.UpdateScore(ToAdd);
             ChangeColor(base_mat);
-            isLightUp = false;
+            ChangeLight();
             audioSource.sound2.Play();
         }
         if (other.gameObject.tag == "Blue" && isLightUp == true) {
@@ -32,7 +33,7 @@ public class JellyfishBehaviour : AJellyfishBehaviour
         if (other.gameObject.tag == "Green" && isLightUp == false && other.gameObject.GetComponent<JellyfishBehaviour>().isLightUp == true) {
             ToAdd = 1;
             score.UpdateScore(ToAdd);
-            isLightUp = true;
+            ChangeLight();
 			audioSource.sound1.Play();
 		}
     }
@@ -44,8 +45,25 @@ public class JellyfishBehaviour : AJellyfishBehaviour
     }
     protected IEnumerator Timer()
     {
+        foreach (GameObject light in lights)
+        {
+            light.GetComponent<Light>().color = new Color32(250, 0, 255, 255);
+        }
         yield return new WaitForSeconds(invincibilityDuration);
         isInvincible = false;
         ChangeColor(lighted_mat);
+		foreach (GameObject light in lights)
+		{
+			light.GetComponent<Light>().color = new Color32(2, 63, 0, 255);
+		}
+	}
+
+    protected void ChangeLight()
+    {
+        foreach (GameObject light in lights)
+        {
+            light.SetActive(!light.activeSelf);
+        }
+        isLightUp = lights[0].activeSelf;
     }
 }
