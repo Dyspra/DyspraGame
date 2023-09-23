eadf7aa109b23bad42f3943be78e80d26ad0b4c1
Jimmy <jimmy.kohl@epitech.eu>
2023-09-24 00:12:02 +0200

    🐛 fix(HandVisualize.cs): fix incorrect variable name for LeftVisualizeContainer ✨ feat(HandVisualize.cs): add functionality to toggle visibility of hand balls and hand plane 🐛 fix(HandVisualize.cs): fix incorrect parent assignment for InitHandPlane method 🐛 fix(HandVisualize.cs): fix incorrect method call for UpdateHandPlane method

--- Assets/Scripts/HandTracking/HandVisualize.cs
+++ Assets/Scripts/HandTracking/HandVisualize.cs
@@ -1,39 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using Mediapipe.Unity;
+using NaughtyAttributes;
 using UnityEngine;
 // using UnityEngine.UI;
 
 
 public class HandVisualize : MonoBehaviour
 {
+    private GameObject LeftVisualizeContainer;
     private GameObject LeftHandBallsContainer;
     private GameObject[] leftHandBalls;
     private GameObject leftPlane;
+    private GameObject RightVisualizeContainer;
     private GameObject RightHandBallsContainer;
     private GameObject[] rightHandBalls;
     private GameObject rightPlane;
     private NewMovementManager MovementManager;
     
     public float smoothingFactor = 10f;
-    // Start is called before the first frame update
+
+    [Button("Toggle Balls", EButtonEnableMode.Playmode)]
+    private void ToggleBalls()
+    {
+        LeftHandBallsContainer.SetActive(!LeftHandBallsContainer.activeSelf);
+        RightHandBallsContainer.SetActive(!RightHandBallsContainer.activeSelf);
+    }
+
+    [Button("Toggle Plane", EButtonEnableMode.Playmode)]
+    private void TogglePlane()
+    {
+        leftPlane.SetActive(!leftPlane.activeSelf);
+        rightPlane.SetActive(!rightPlane.activeSelf);
+    }
 
     void Start()
     {
         MovementManager = this.GetComponent<NewMovementManager>();
-        LeftHandBallsContainer = InitContainer("LeftVisualizeContainer");
-        RightHandBallsContainer = InitContainer("RightVisualizeContainer");
+        LeftVisualizeContainer = InitContainer("LeftVisualizeContainer");
+        LeftHandBallsContainer = InitContainer("LeftHandBallsContainer", LeftVisualizeContainer.transform);
+        LeftHandBallsContainer.SetActive(false);
+        RightVisualizeContainer = InitContainer("RightVisualizeContainer");
+        RightHandBallsContainer = InitContainer("RightHandBallsContainer", RightVisualizeContainer.transform);
+        RightHandBallsContainer.SetActive(false);
         InitHandBalls(ref leftHandBalls, LeftHandBallsContainer);
-        InitHandPlane(ref leftPlane, LeftHandBallsContainer);
+        InitHandPlane(ref leftPlane, LeftVisualizeContainer);
+        leftPlane.SetActive(false);
         InitHandBalls(ref rightHandBalls, RightHandBallsContainer);
-        InitHandPlane(ref rightPlane, RightHandBallsContainer);
-        // InitWebCamScreen();
+        InitHandPlane(ref rightPlane, RightVisualizeContainer);
+        rightPlane.SetActive(false);
     }
 
-    private GameObject InitContainer(string containerName)
+    private GameObject InitContainer(string containerName, Transform parent = null)
     {
         GameObject container = new GameObject(containerName);
-        container.transform.parent = this.transform;
+        if (parent != null)
+        {
+            container.transform.parent = parent;
+        }
+        else
+        {
+            container.transform.parent = this.transform;
+        }
         container.transform.localPosition = new Vector3(0, 0, 0);
         return container;
     }
@@ -50,10 +78,10 @@ public class HandVisualize : MonoBehaviour
         }
     }
 
-    private void InitHandPlane(ref GameObject plane, GameObject handBallsContainer)
+    private void InitHandPlane(ref GameObject plane, GameObject parent)
     {
         plane = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        plane.transform.parent = handBallsContainer.transform;
+        plane.transform.parent = parent.transform;
         plane.transform.localPosition = new Vector3(0, 0, 0);
         plane.transform.localScale = new Vector3(0.3f, 0.3f, 0.0001f);
         plane.transform.localRotation = Quaternion.Euler(90, 0, 0);
@@ -63,13 +91,22 @@ public class HandVisualize : MonoBehaviour
     // Update is called once per frame
     public void Update()
     {
-        ApplyHandPosition(LeftHandBallsContainer.transform, HandTrackingManager.Instance.HandTracking.LeftHandPosition);
-        ApplyBallsPosition(leftHandBalls, HandTrackingManager.Instance.HandTracking.LeftHandLandmarks);
-        UpdateHandPlane(leftPlane, MovementManager.leftHandRotation, HandTrackingManager.Instance.HandTracking.LeftHandPosition);
+        if (LeftHandBallsContainer.activeSelf)
+        {
+            ApplyHandPosition(LeftHandBallsContainer.transform, HandTrackingManager.Instance.HandTracking.LeftHandPosition);
+            ApplyBallsPosition(leftHandBalls, HandTrackingManager.Instance.HandTracking.LeftHandLandmarks);
+        }
+        if (RightHandBallsContainer.activeSelf)
+        {
+            ApplyHandPosition(RightHandBallsContainer.transform, HandTrackingManager.Instance.HandTracking.RightHandPosition);
+            ApplyBallsPosition(rightHandBalls, HandTrackingManager.Instance.HandTracking.RightHandLandmarks);
+        }
 
-        ApplyHandPosition(RightHandBallsContainer.transform, HandTrackingManager.Instance.HandTracking.RightHandPosition);
-        ApplyBallsPosition(rightHandBalls, HandTrackingManager.Instance.HandTracking.RightHandLandmarks);
-        UpdateHandPlane(rightPlane, MovementManager.rightHandRotation, HandTrackingManager.Instance.HandTracking.RightHandPosition);
+        if (leftPlane.activeSelf)
+        {
+            UpdateHandPlane(leftPlane, MovementManager.leftHandRotation, HandTrackingManager.Instance.HandTracking.LeftHandPosition);
+            UpdateHandPlane(rightPlane, MovementManager.rightHandRotation, HandTrackingManager.Instance.HandTracking.RightHandPosition);
+        }
     }
 
     private void ApplyBallsPosition(GameObject[] handBalls, Vector3[] handLandmarks)
@@ -85,14 +122,9 @@ public class HandVisualize : MonoBehaviour
 
     private void ApplyHandPosition(Transform hand, Vector3 handPosition)
     {
-        // handPosition.x -= 0.5f;
-        // handPosition.y += 1f;
         hand.localPosition = Vector3.Lerp(hand.transform.localPosition, handPosition, Time.deltaTime * smoothingFactor);
     }
 
-    // plane
-    // Quaternion handRotation 
-    // handPosition
     private void UpdateHandPlane(GameObject plane, Quaternion handRotation, Vector3 handPosition)
     {
         plane.transform.localRotation = Quaternion.Lerp(plane.transform.localRotation, handRotation, Time.deltaTime * smoothingFactor);
