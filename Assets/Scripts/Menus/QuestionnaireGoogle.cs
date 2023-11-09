a9aa23531a9e27a08f82597909df8a4cfa422a66
titouandelpech <63298260+titouandelpech@users.noreply.github.com>
2023-11-09 09:17:01 +0100

    [ADD] Fix form player pref

--- Assets/Scripts/Menus/QuestionnaireGoogle.cs
+++ Assets/Scripts/Menus/QuestionnaireGoogle.cs
@@ -2,34 +2,31 @@ using UnityEngine;
 
 public class QuestionnaireGoogle : MonoBehaviour
 {
-
     [SerializeField] private GameObject m_windowExercices;
-    //[SerializeField] private GameObject m_windowTutoForm;
     [SerializeField] private GameObject m_windowForms;
 
-    //private int m_currentPlayerPrefHasClickedOnForm = 0;
+    private int m_currentPlayerPrefHasClickedOnForm = 0; //0: default, 1: played but didn't open form menu, 2: opened form
 
-    //private void Start()
-    //{
-    //    m_currentPlayerPrefHasClickedOnForm = PlayerPrefs.GetInt("HasClickedOnForm");
-    //    Debug.Log("Current click = " + m_currentPlayerPrefHasClickedOnForm);
+    private void Start()
+    {
+        m_currentPlayerPrefHasClickedOnForm = PlayerPrefs.GetInt("HasClickedOnForm");
+        Debug.Log("Current click = " + m_currentPlayerPrefHasClickedOnForm);
 
-    //    if (m_currentPlayerPrefHasClickedOnForm == 1)
-    //    {
-    //        m_mainCanvas.SetActive(false);
-    //        m_windowFormAsked.SetActive(true);
-    //        m_windowTutoForm.SetActive(false);
-    //    }
-    //}
+        if (m_currentPlayerPrefHasClickedOnForm == 1)
+        {
+            m_windowExercices.SetActive(false);
+            m_windowForms.SetActive(true);
+        }
+    }
 
-    //private void Update()
-    //{
-    //    if (Input.GetKeyDown("space"))
-    //    {
-    //        m_currentPlayerPrefHasClickedOnForm = 0;
-    //        PlayerPrefs.SetInt("HasClickedOnForm", m_currentPlayerPrefHasClickedOnForm);
-    //    }
-    //}
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            m_currentPlayerPrefHasClickedOnForm = 0;
+            PlayerPrefs.SetInt("HasClickedOnForm",m_currentPlayerPrefHasClickedOnForm);
+        }
+    }
 
     public void OpenDyspraxicForm()
     {
@@ -45,37 +42,18 @@ public class QuestionnaireGoogle : MonoBehaviour
 
     private void ClickedOnForm()
     {
-        //if (m_currentPlayerPrefHasClickedOnForm != 2)
-        //{
-        //    m_currentPlayerPrefHasClickedOnForm = 2;
-        //    PlayerPrefs.SetInt("HasClickedOnForm", 2);
-        //}
-        m_windowExercices.SetActive(true);
-        m_windowForms.SetActive(false);
+        if (m_currentPlayerPrefHasClickedOnForm != 2)
+        {
+            m_currentPlayerPrefHasClickedOnForm = 2;
+            PlayerPrefs.SetInt("HasClickedOnForm", 2);
+        }
     }
 
-    //public void OnClickedOnReturnButton()
-    //{
-    //    if (m_mainCanvas == null || m_windowTutoForm == null)
-    //        return;
-
-    //    if (m_currentPlayerPrefHasClickedOnForm != 2)
-    //    {
-    //        m_windowTutoForm.SetActive(true);
-    //        m_mainCanvas.SetActive(false);
-    //        m_windowFormAsked.SetActive(false);
-    //        PlayerPrefs.SetInt("HasClickedOnForm", 0);
-    //    } else
-    //    {
-    //        m_mainCanvas.SetActive(true);
-    //    }
-    //}
-
-    //public void OnClickedOnGoToFormWindow()
-    //{
-    //    if (m_currentPlayerPrefHasClickedOnForm != 2)
-    //    {
-    //        PlayerPrefs.SetInt("HasClickedOnForm", 0);
-    //    }
-    //}
+    public void OnClickedGame()
+    {
+        if (m_currentPlayerPrefHasClickedOnForm != 2)
+        {
+            PlayerPrefs.SetInt("HasClickedOnForm", 1);
+        }
+    }
 }
