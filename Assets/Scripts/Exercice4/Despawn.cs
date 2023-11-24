039c7cac680435f6687b62997b9c921de2f44eb2
Campidoctoris <etienne1.delrieux@epitech.eu>
2023-11-24 17:06:55 +0100

    [ADD] Despawn Zone to destroy ingredients

---
+++ Assets/Scripts/Exercice4/Despawn.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Despawn : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collide");
+        Ingredients ingredients;
+        if (other.gameObject.TryGetComponent<Ingredients>(out ingredients) == true)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
