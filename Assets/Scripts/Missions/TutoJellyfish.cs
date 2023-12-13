e54f12960297bb39aed26bc19dc49f0fdeca36d4
Thomas Billaud <thomas.billaud@epitech.eu>
2023-12-13 11:28:30 +0100

    [ADD] Tutorial plays in exercice 2 after choosing game duration

---
+++ Assets/Scripts/Missions/TutoJellyfish.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutoJellyfish : MonoBehaviour
+{
+	public List<GameObject> tutorials;
+	public GameObject gameUI;
+	public GameObject jellyfishSpawner;
+	int tutoNb = 0;
+
+	private void OnEnable()
+	{
+		tutorials[tutoNb].SetActive(true);
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyUp(KeyCode.Space))
+		{
+			if (tutoNb + 1 < tutorials.Count)
+			{
+				tutorials[tutoNb].SetActive(false);
+				tutoNb++;
+				tutorials[tutoNb].SetActive(true);
+			} else 
+			{
+				gameUI.SetActive(true);
+				jellyfishSpawner.SetActive(true);
+				gameObject.SetActive(false);
+			}
+		}
+	}
+}
