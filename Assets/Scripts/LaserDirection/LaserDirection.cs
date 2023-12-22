86cce0bb7b3c31b365adbbd77d57682e99c3f5f1
Baptiste ARRECOT <67862732+baptiste-arrecot@users.noreply.github.com>
2023-12-22 11:16:17 +0100

    [CHANGE] Changed the laser power to make it more intuitive

--- Assets/Scripts/LaserDirection/LaserDirection.cs
+++ Assets/Scripts/LaserDirection/LaserDirection.cs
@@ -5,19 +5,26 @@ using UnityEngine;
 public class LaserDirection : MonoBehaviour
 {
     public GameObject origin;
+    public GameObject laserDisplay;
     public GameObject laserDirection;
     public LayerMask collisionLayer;
     LineRenderer laser;
 
+    public float[] offset;
+
     private void Update()
     {
-        RaycastHit hit;
-
+        Vector3 newDisplay = new Vector3(30.31f * (origin.transform.position.x) + offset[0], 30.31f * (origin.transform.position.y) + offset[1], laserDirection.transform.position.z);
+        Vector3 newPos = new Vector3(origin.transform.position.x, origin.transform.position.y, laserDirection.transform.position.z);
+        laserDisplay.transform.position = newDisplay;
+        laserDirection.transform.position = newPos;
+        transform.position = newDisplay;
+    }
+}
+        /*RaycastHit hit;
         if (Physics.Raycast(origin.transform.position, origin.transform.right, out hit, Mathf.Infinity, collisionLayer))
         {
             laserDirection.transform.position = hit.point;
             Vector3 hitPoint = hit.point;
             transform.position = hitPoint;
-        }
-    }
-}
+        }*/
