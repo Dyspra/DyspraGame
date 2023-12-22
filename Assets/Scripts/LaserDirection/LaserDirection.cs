856002546c8b59b19c2d57dd10235f51aa9dcb96
Thomas Billaud <thomas.billaud@epitech.eu>
2023-12-22 16:29:55 +0100

    [MOD] Same thing as Baptiste but with the raycast

--- Assets/Scripts/LaserDirection/LaserDirection.cs
+++ Assets/Scripts/LaserDirection/LaserDirection.cs
@@ -6,7 +6,6 @@ public class LaserDirection : MonoBehaviour
 {
     public GameObject origin;
     public GameObject laserDisplay;
-    public GameObject laserDirection;
     public LayerMask collisionLayer;
     LineRenderer laser;
 
@@ -14,17 +13,18 @@ public class LaserDirection : MonoBehaviour
 
     private void Update()
     {
-        Vector3 newDisplay = new Vector3(30.31f * (origin.transform.position.x) + offset[0], 30.31f * (origin.transform.position.y) + offset[1], laserDirection.transform.position.z);
+        /*Vector3 newDisplay = new Vector3(30.31f * (origin.transform.position.x) + offset[0], 30.31f * (origin.transform.position.y) + offset[1], laserDirection.transform.position.z);
         Vector3 newPos = new Vector3(origin.transform.position.x, origin.transform.position.y, laserDirection.transform.position.z);
         laserDisplay.transform.position = newDisplay;
         laserDirection.transform.position = newPos;
-        transform.position = newDisplay;
+        transform.position = newDisplay;*/
+        RaycastHit hit;
+        if (Physics.Raycast(origin.transform.position, Vector3.forward, out hit, Mathf.Infinity, collisionLayer))
+        {
+            float dist = Vector3.Distance(origin.transform.position, hit.point);
+            laserDisplay.transform.position = new Vector3(hit.point.x * dist, hit.point.y * dist, hit.point.z);
+            Vector3 hitPoint = hit.point;
+            transform.position = laserDisplay.transform.position;
+		}
     }
 }
-        /*RaycastHit hit;
-        if (Physics.Raycast(origin.transform.position, origin.transform.right, out hit, Mathf.Infinity, collisionLayer))
-        {
-            laserDirection.transform.position = hit.point;
-            Vector3 hitPoint = hit.point;
-            transform.position = hitPoint;
-        }*/
