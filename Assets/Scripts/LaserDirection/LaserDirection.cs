69259770b89bed3e38fad2808693fa86e479b32a
Baptiste ARRECOT <67862732+baptiste-arrecot@users.noreply.github.com>
2024-01-03 14:43:03 +0100

    [CHANGE] tests on the offset dynamic of the laser

--- Assets/Scripts/LaserDirection/LaserDirection.cs
+++ Assets/Scripts/LaserDirection/LaserDirection.cs
@@ -8,7 +8,6 @@ public class LaserDirection : MonoBehaviour
     public GameObject laserDisplay;
     public LayerMask collisionLayer;
     LineRenderer laser;
-
     public float[] offset;
 
     private void Update()
@@ -19,11 +18,23 @@ public class LaserDirection : MonoBehaviour
         laserDirection.transform.position = newPos;
         transform.position = newDisplay;*/
         RaycastHit hit;
+        float x, y;
+        Vector2 screenBoundaries = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
         if (Physics.Raycast(origin.transform.position, Vector3.forward, out hit, Mathf.Infinity, collisionLayer))
         {
             float dist = Vector3.Distance(origin.transform.position, hit.point);
-            laserDisplay.transform.position = new Vector3(hit.point.x * dist, hit.point.y * dist, hit.point.z);
-            Vector3 hitPoint = hit.point;
+            //x =  (hit.point.x + screenBoundaries.x /  (screenBoundaries.x *2)) * (screenBoundaries.x *2) - screenBoundaries.x;
+            //y =  (hit.point.y + screenBoundaries.y /  (screenBoundaries.y *2)) * (screenBoundaries.y *2) - screenBoundaries.y;
+            //Debug.Log("Fingerx = " + origin.transform.position.x + " Fingery = " + origin.transform.position.y);
+            //Debug.Log("x = " + hit.point.x + " y = " + hit.point.y);
+            //Vector3 point = Camera.main.ScreenToWorldPoint(Camera.main.WorldToScreenPoint(new Vector3(hit.point.x, hit.point.y, 0)));
+            Vector3 planeNormal = Vector3.forward;
+            Vector3 point = Vector3.ProjectOnPlane(origin.transform.position, planeNormal);
+            point.z = 0;
+            /*Matrix4x4 translationMatrix = Matrix4x4.Translate(new Vector3(0.0f, 0.0f, hit.point.z - origin.transform.position.z));
+            translationMatrix.MultiplyPoint3x4(origin.transform.position);*/
+            laserDisplay.transform.position = /*point;*/new Vector3(point.x * dist /*+ offset[0]*/, point.y * dist /*+ offset[1]*/, 0);
+            Vector3 hitPoint = point;
             transform.position = laserDisplay.transform.position;
 		}
     }
